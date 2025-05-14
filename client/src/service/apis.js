39fbf6b42a1c62a13067e38bade9e3b58ac33a0e
Abhishek Kumar Sharma <akankshasharma47380@gmail.com>
2025-05-14 09:53:33 +0530

    removing localhost from baseurl for deployed version

--- client/src/service/apis.js
+++ client/src/service/apis.js
@@ -1,12 +1,13 @@
-const baseURL = `${import.meta.env.VITE_BASE_URL}/api/v1`;
+const baseURL = `/api/v1`;
+// const baseURL = `${import.meta.env.VITE_BASE_URL}/api/v1`;
 
 export const endpoints = {
-  login: `${baseURL}/auth/login`,
-  signup: `${baseURL}/auth/signup`,
-  addBook: `${baseURL}/book/addBook`,
-  getAvailableBooks: `${baseURL}/book/getAvailableBooks`,
-  borrowBook: `${baseURL}/transaction/borrow`,
-  returnBook: `${baseURL}/transaction/return`,
-  getBooks: `${baseURL}/book/getBooks`,
-  getIssuedBooks: `${baseURL}/book/getIssuedBooks`,
+   login: `${baseURL}/auth/login`,
+   signup: `${baseURL}/auth/signup`,
+   addBook: `${baseURL}/book/addBook`,
+   getAvailableBooks: `${baseURL}/book/getAvailableBooks`,
+   borrowBook: `${baseURL}/transaction/borrow`,
+   returnBook: `${baseURL}/transaction/return`,
+   getBooks: `${baseURL}/book/getBooks`,
+   getIssuedBooks: `${baseURL}/book/getIssuedBooks`,
 };
